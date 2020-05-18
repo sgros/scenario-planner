53a9e4b158664b4b31883ffe5181a8d689a3a17b
Krmpi <maja.krmpotic96@gmail.com>
2020-05-18 10:21:51 +0200

    Fixed some issues and cleaned the code

--- server/server.py
+++ server/server.py
@@ -5,16 +5,11 @@ from pymongo import MongoClient
 from datetime import datetime
 from bson import json_util
 from flask_cors import CORS
-import logging
 
 app = Flask(__name__)
 client = MongoClient('mongo', 27017)
 db = client.db
 CORS(app)
-#app.config['MONGODB_NAME'] = 'gantt_tasks'
-#app.config['MONGO_URI'] = 'mongodb://localhost:27017/gantt_tasks'
-
-#mongo = PyMongo(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -22,9 +17,9 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/', methods=['GET'])
 @cross_origin()
 def root():
-	db.hits.insert_one({ 'time': datetime.utcnow() })
-	message = 'This page has been visited {} times.'.format(db.hits.count())
-	return jsonify({ 'message': message })
+    db.hits.insert_one({ 'time': datetime.utcnow() })
+    message = 'This page has been visited {} times.'.format(db.hits.count())
+    return jsonify({ 'message': message })
 
 
 @app.route('/time')
@@ -32,21 +27,27 @@ def root():
 def get_current_time():
     return jsonify({ 'time': time.time() })
 
+
 @app.route('/gantt/task', methods=['POST'])
 @cross_origin()
 def create_task():
-    # task = {
-    #     'taskid': 2,#getNextSequence(db.tasks,"taskid"),
-    #     'text': request.json['text'],
-    #     'start_date': request.json['start_date'],
-    #     'end_date': request.json['end_date'],
-    #     'holder': request.json['holder'],
-    #     'action': request.json['action'],
-    #     'priority': request.json['priority'],
-    #     'done': False
-    # }
-    # db.tasks.insert(task)
-    return jsonify({"action":"inserted", "tid":2})
+    task = {
+        'taskid': get_next_sequence(db.tasks,"taskid"),
+        'text': request.form["text"],
+        'start_date': request.form["start_date"],
+        'end_date': request.form["end_date"],
+        'holder': request.form["holder"],
+        'action': request.form["action"],
+        'priority': request.form["priority"],
+        'progress': request.form["progress"],
+        'parent': request.form["parent"],
+        'duration': request.form["duration"],
+        'done': False
+    }
+
+    print(task, flush=True)
+    db.tasks.insert(task)
+    return jsonify({"action":"inserted", "taskid":task['taskid']})
 
 
 @app.route('/gantt', methods=['GET'])
@@ -55,15 +56,18 @@ def get_tasks():
     tasks = [task for task in db.tasks.find({})]
     return json_util.dumps({'tasks': tasks})
 
+
 @app.route('/gantt/task/<taskId>', methods=['PUT'])
 @cross_origin()
 def update_task(taskId):
     return jsonify({"action":"updated"})
 
+
 @app.route('/gantt/task/<taskId>', methods=['DELETE'])
 @cross_origin()
 def delete_task(taskId):
     return jsonify({"action":"deleted"})
+
 
 @app.route('/gantt/link', methods=['POST'])
 @cross_origin()
@@ -83,15 +87,15 @@ def delete_link(linkId):
     return jsonify({"action": "deleted"})
 
 
-
-def getNextSequence(collection,name):
-   sequence = collection.find_and_modify(query={'_id': name}, update={'$inc': {'seq': 1}}, new=True)
-
-   if (sequence == None):
-       return 2
-   return sequence.get('seq');
+def get_next_sequence(collection, name):
+    # sequence = collection.find_and_modify(query={'_id': name}, update={'$inc': {'taskid': 1}}, new=True)
+    sequence = collection.find_and_modify(update={'$inc': {'taskid': 1}}, new=True)
+    print(sequence, flush=True)
+    if sequence is None:
+        return 1
+    return sequence.get('taskid')
 
 
 if __name__ == '__main__':
-	# only used locally
-	app.run(host='0.0.0.0', port=8080, debug=True)
+    # only used locally
+    app.run(host='0.0.0.0', port=8080, debug=True)
